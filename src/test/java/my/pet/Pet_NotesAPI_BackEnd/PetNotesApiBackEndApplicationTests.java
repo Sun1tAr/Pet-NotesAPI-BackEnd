1fe7db30273ff37b596982d4624a4770ebe3f714
Denis Tuev <tuev_dd@nectech.pro>
2026-01-19 15:47:07 +0300

    adds build action

--- src/test/java/my/pet/Pet_NotesAPI_BackEnd/PetNotesApiBackEndApplicationTests.java
+++ src/test/java/my/pet/Pet_NotesAPI_BackEnd/PetNotesApiBackEndApplicationTests.java
@@ -3,11 +3,14 @@ package my.pet.Pet_NotesAPI_BackEnd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class PetNotesApiBackEndApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
