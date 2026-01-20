74f026922454a3682c5ae6a0490842991fcbe474
Denis Tuev <tuev_dd@nectech.pro>
2026-01-20 14:49:14 +0300

    CORS off

--- src/main/java/my/pet/Pet_NotesAPI_BackEnd/config/WebConfig.java
+++ src/main/java/my/pet/Pet_NotesAPI_BackEnd/config/WebConfig.java
@@ -3,6 +3,7 @@ package my.pet.Pet_NotesAPI_BackEnd.config;
 import lombok.RequiredArgsConstructor;
 import my.pet.Pet_NotesAPI_BackEnd.interceptor.LoggingInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,4 +21,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(logger)
         .addPathPatterns("/**");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")           // для всех эндпоинтов
+                .allowedOrigins("*")           // доступ есть с любых хостов
+                .allowedMethods("*")           // для всех методов HTTP
+                .allowedHeaders("*")           // в запросе можно ставить любые заголовки
+                .maxAge(3600);                 // браузер может час не отправлять Options
+    }
+
 }
